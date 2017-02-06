99200c5161280f39be9cf4a0b869cc64fae243b3
wulong yang <yangwulong0418@gmail.com>
2017-02-06 14:29:48 +1100

    Initial commit

---
+++ app/src/main/java/q6/com/au/q6androidclient/LoginActivity.java
@@ -0,0 +1,13 @@
+package q6.com.au.q6androidclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
